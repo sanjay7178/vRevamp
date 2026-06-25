ca4ff234357c4dcb1b64261c9e0a741f4cc25390
chanu0073 <chanu6692@gmail.com>
2026-06-26 03:02:32 +0530

    refactor: move storeCreditsArrays to store.js, delete saveCreditsDetails.js

--- js/store/store.js
+++ js/store/store.js
@@ -44,3 +44,24 @@ function getData(key, callback) {
     }
   });
 }
+
+function storeCreditsArrays(semester, courseCodes, credits, callback) {
+  if (!semester || !courseCodes || !credits) {
+    if (callback) callback();
+    return;
+  }
+
+  getData("creditsBySemester", (existingData) => {
+    const allCredits = existingData || {};
+
+    allCredits[semester] = {
+      courseCodes,
+      credits
+    };
+
+    saveData("creditsBySemester", allCredits, () => {
+      // console.log("Credits stored for semester:", semester);
+      if (callback) callback();
+    });
+  });
+}
